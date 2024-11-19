6cf9de6b1aaab12350f3bd46158747c761ffeb0b
Harshalekkala <105717718+lekkalaharsha@users.noreply.github.com>
2024-11-19 20:09:37 +0530

    new final version 1.0.7

--- lib/screens/constapi.dart
+++ lib/screens/constapi.dart
@@ -1,2 +1,2 @@
 // ignore: constant_identifier_names
-const String GEMINI_API_KEY = "AIzaSyAakbrDzgchxZ5zGaY3wsl-dWycbdAL4Tw";
+const String GEMINI_API_KEY = "AIzaSyCEaJJJ3O7ghAiY1qa-BrdKXqKBwCoE_do";
